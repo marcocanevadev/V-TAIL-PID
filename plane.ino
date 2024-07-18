7695740c59e697764cbd80cb1ac81108edaf4ac2
marco caneva <marco.caneva.dev@outlook.com>
2024-07-18 11:41:04 +0200

    fixed wrong LICENSE

--- plane.ino
+++ plane.ino
@@ -2,27 +2,22 @@
   This is the V-TAIL-PID code.
   Copyright (c) 2024 Marco Caneva. All rights reserved.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-    
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  he Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-  or go to: https://www.gnu.org/licenses/gpl-3.0.txt
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
   For inquiries, please contact:
-
   Marco Caneva
-  
   Email:  marco.caneva@example.com
-
   Mail:   Marco Caneva
           Via Dei Ciclamini 33
           20147 Milan (MI)
